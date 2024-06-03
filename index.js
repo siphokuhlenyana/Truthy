290b0e327bbdf7cc0e37874de157e32b17eab406
Siphokuhle <siphokuhlenyana@gmail.com>
2024-06-03 15:56:32 +0200

    added

---
+++ index.js
@@ -0,0 +1,10 @@
+let checkTruthy=[true ,false,false ,true ,true]
+let countTrue(checkTruthy){
+    return checkTruthy.count(true)
+}
+function check(...checkTruthy){
+    if (checkTruthy == true){
+   console.log()
+    }
+    }
+    check()
